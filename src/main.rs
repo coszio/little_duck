8d59201f4a4f7719b93f8dccd7d643daf615dae8
Luis Cossío <luis.cossio@outlook.com>
2022-03-15 21:30:21 -0600

    added tests

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,12 @@ fn main() {
 
 #[test]
 fn hello_world() {
-    let good_str = "program myProgram: 
+    let good_str = "program myProgram; 
     { 
         print(cte.string); 
     }";
     let bad_str1 = "program myProgram { print(cte.string); }";
-    let bad_str2 = "program myProgram: { print(cte.string) }";
+    let bad_str2 = "program myProgram; { print(cte.string) }";
     
     assert!(little_duck::PROGRAMAParser::new().parse(good_str).is_ok());
     assert!(little_duck::PROGRAMAParser::new().parse(bad_str1).is_err());
@@ -25,7 +25,7 @@ fn hello_world() {
 
 #[test]
 fn condicion() {
-    let test_str = "program myProgram:
+    let test_str = "program myProgram;
     {
         if (a > b) {
             a = b;
@@ -34,7 +34,7 @@ fn condicion() {
         };
     }";
 
-    let bad_str1 = "program myProgram:
+    let bad_str1 = "program myProgram;
     {
         if (a > b) {
             a = b;
@@ -43,7 +43,7 @@ fn condicion() {
         }
     }";
 
-    let bad_str2 = "program myProgram:
+    let bad_str2 = "program myProgram;
     {
         if a > b {
             a = b;
@@ -54,5 +54,30 @@ fn condicion() {
     assert!(little_duck::PROGRAMAParser::new().parse(test_str).is_ok());
     assert!(little_duck::PROGRAMAParser::new().parse(bad_str1).is_err());
     assert!(little_duck::PROGRAMAParser::new().parse(bad_str2).is_err());
+}
 
+
+#[test]
+fn instanciar_variables() {
+    let good_str1 = "program myProgram; 
+    var a: int;
+    { 
+        print(cte.string); 
+    }";
+    
+    let good_str2 = "program myProgram; 
+    var b: float;
+    { 
+        print(cte.string); 
+    }";
+
+    
+    
+    let bad_str1 = "program myProgram { print(cte.string); }";
+    let bad_str2 = "program myProgram: { print(cte.string) }";
+    
+    assert!(little_duck::PROGRAMAParser::new().parse(good_str1).is_ok());
+    assert!(little_duck::PROGRAMAParser::new().parse(good_str2).is_ok());
+    assert!(little_duck::PROGRAMAParser::new().parse(bad_str1).is_err());
+    assert!(little_duck::PROGRAMAParser::new().parse(bad_str2).is_err());
 }
